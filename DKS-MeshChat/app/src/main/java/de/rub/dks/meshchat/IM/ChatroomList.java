4ba73f4437f553b802b27e117e55500938bcfa26
Max <max-hoffmann@gmx.de>
2015-03-19 19:29:09 +0100

    Added Documentation

--- DKS-MeshChat/app/src/main/java/de/rub/dks/meshchat/IM/ChatroomList.java
+++ DKS-MeshChat/app/src/main/java/de/rub/dks/meshchat/IM/ChatroomList.java
@@ -10,12 +10,19 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 import de.rub.dks.meshchat.R;
 
+/**
+* Adapter class, managing all the chatrooms and displayable in a list.
+*/
 public class ChatroomList extends BaseAdapter {
 	public static final String DEFAULT_CHAT_ROOM = "All";
 	private Context context;
 	private ArrayList<String> rooms;
 	private LayoutInflater inflater;
 
+	/**
+	* Constructor.
+	* @param pContext the parent context
+	*/
 	public ChatroomList(Context pContext) {
 		context = pContext;
 		rooms = new ArrayList<String>();
@@ -26,11 +33,9 @@ public class ChatroomList extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View itemView = inflater.inflate(R.layout.drawer_list_item, parent, false);
-
 		TextView txtTitle = (TextView) itemView.findViewById(R.id.title);
-
+		// display the rooms title w/ a little bit of space in front
 		txtTitle.setText("    "+rooms.get(position));
-
 		return itemView;
 	}
 
